94a8a3c945cce72018c0e81c436071ddd2dc542b
jojiang <jojiang@easyops.cn>
2021-04-07 19:49:34 +0800

    fix(): modify sdk
    Refs USER_ADMIN-125

--- sdk/air-admin-service-sdk/src/api/air_admin_service/customer/getExpiration.ts
+++ sdk/air-admin-service-sdk/src/api/air_admin_service/customer/getExpiration.ts
@@ -1,11 +1,6 @@
 import { http, HttpOptions } from "@next-core/brick-http";
 import { ResponseBodyWrapper } from "../../../wrapper";
 
-export interface GetExpirationRequestParams {
-  /** EASYOPS_AIR_CUSTOMER_ORG模型的实例id */
-  instanceId: string;
-}
-
 export interface GetExpirationResponseBody {
   /** 过期时间戳 */
   expires: number;
@@ -16,12 +11,11 @@ export interface GetExpirationResponseBody {
  * @endpoint GET /api/v1/customer/expiration
  */
 export const getExpiration = async (
-  params: GetExpirationRequestParams,
   options?: HttpOptions
 ): Promise<GetExpirationResponseBody> =>
   (
     await http.get<ResponseBodyWrapper<GetExpirationResponseBody>>(
       "api/gateway/air_admin_service.customer.GetExpiration/api/v1/customer/expiration",
-      { ...options, params }
+      options
     )
   ).data;
