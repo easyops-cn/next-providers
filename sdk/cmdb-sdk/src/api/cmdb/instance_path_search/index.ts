33652ea04465df11b21569d5bff4b11946c2b4fd
easyops-eve <easyops-eve@users.noreply.github.com>
2021-07-21 17:57:44 +0800

    chore(): update sdk

--- sdk/cmdb-sdk/src/api/cmdb/instance_path_search/index.ts
+++ sdk/cmdb-sdk/src/api/cmdb/instance_path_search/index.ts
@@ -1,3 +1,5 @@
+export * from "./flattenSearchPath";
+export * from "./groupSearchPath";
 export * from "./pathSearchExportV3";
 export * from "./pathSearchExportV4";
 export * from "./pathSearchV3";
