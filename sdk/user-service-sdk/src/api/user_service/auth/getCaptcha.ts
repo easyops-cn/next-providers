db60ce961a5d0b8f065c6cc3bc1978f0d366fdbc
easyops-eve <easyops-eve@users.noreply.github.com>
2021-08-05 09:51:25 +0800

    chore(): update sdk

--- sdk/user-service-sdk/src/api/user_service/auth/getCaptcha.ts
+++ sdk/user-service-sdk/src/api/user_service/auth/getCaptcha.ts
@@ -4,8 +4,8 @@ import { http, HttpOptions } from "@next-core/brick-http";
  * @description 登录验证码[内部]
  * @endpoint GET /api/v1/users/captcha
  */
-export const AuthApi_getCaptcha = (options?: HttpOptions): Promise<void> =>
-  /**! @contract easyops.api.user_service.auth.GetCaptcha */ http.get<void>(
+export const AuthApi_getCaptcha = (options?: HttpOptions): Promise<Blob> =>
+  /**! @contract easyops.api.user_service.auth.GetCaptcha */ http.get<Blob>(
     "api/gateway/user_service.auth.GetCaptcha/api/v1/users/captcha",
-    options
+    { ...options, responseType: "blob" }
   );
