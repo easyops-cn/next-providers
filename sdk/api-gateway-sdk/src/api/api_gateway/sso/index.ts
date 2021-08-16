cae37c70fb844a05261522808505080842cef3c4
easyops-eve <easyops-eve@users.noreply.github.com>
2021-08-16 13:37:37 +0800

    chore(): update sdk

--- sdk/api-gateway-sdk/src/api/api_gateway/sso/index.ts
+++ sdk/api-gateway-sdk/src/api/api_gateway/sso/index.ts
@@ -1,6 +1,7 @@
 export * from "./samlAuthorization";
 export * from "./samlLogout";
 export * from "./ssoAuthorization";
+export * from "./ssoAuthorizationToken";
 export * from "./ssoLogin";
 export * from "./ssoLogout";
 export * from "./ssoLogoutV2";
