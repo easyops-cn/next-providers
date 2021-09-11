25b60f5e6703fb201fcc7123e84b8fed8dbf1a0e
easyops-eve <easyops-eve@users.noreply.github.com>
2021-09-11 23:05:42 +0800

    chore(): update sdk

--- sdk/permission-sdk/src/api/permission/menu/listLegacyModule.ts
+++ sdk/permission-sdk/src/api/permission/menu/listLegacyModule.ts
@@ -1,12 +1,12 @@
 import { http, HttpOptions } from "@next-core/brick-http";
-import { ResponseListWrapper } from "../../../wrapper";
+import { ResponseBodyWrapper } from "../../../wrapper";
 
 export interface MenuApi_ListLegacyModuleRequestParams {
   /** 用户使用的平台(例如cmdb,ops), CheckLogin 会返回此字段 */
   loginRegion?: string;
 }
 
-export interface MenuApi_ListLegacyModuleResponseItem {
+export interface MenuApi_ListLegacyModuleResponseBody {
   /** 模块 remark 列表, 只可能是以下枚举
  持续部署
  运维自动化
@@ -22,18 +22,17 @@ export interface MenuApi_ListLegacyModuleResponseItem {
   menus?: string[];
 }
 
-export type MenuApi_ListLegacyModuleResponseBody =
-  ResponseListWrapper<MenuApi_ListLegacyModuleResponseItem>;
-
 /**
  * @description 获取模块权限
- * @endpoint LIST /api/v1/permission/module/legacy
+ * @endpoint GET /api/v1/permission/module/legacy
  */
-export const MenuApi_listLegacyModule = (
+export const MenuApi_listLegacyModule = async (
   params: MenuApi_ListLegacyModuleRequestParams,
   options?: HttpOptions
 ): Promise<MenuApi_ListLegacyModuleResponseBody> =>
-  /**! @contract easyops.api.permission.menu.ListLegacyModule */ http.get<MenuApi_ListLegacyModuleResponseBody>(
-    "api/gateway/permission.menu.ListLegacyModule/api/v1/permission/module/legacy",
-    { ...options, params }
-  );
+  /**! @contract easyops.api.permission.menu.ListLegacyModule */ (
+    await http.get<ResponseBodyWrapper<MenuApi_ListLegacyModuleResponseBody>>(
+      "api/gateway/permission.menu.ListLegacyModule/api/v1/permission/module/legacy",
+      { ...options, params }
+    )
+  ).data;
