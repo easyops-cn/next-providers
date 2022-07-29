76121f7aa1cc43f3041cade3725eaca798f03a7f
李佳明 <jimmyli@easyops.cn>
2022-07-29 16:20:00 +0800

    feat(): api gateway sdk

--- sdk/api-gateway-sdk/src/index.ts
+++ sdk/api-gateway-sdk/src/index.ts
@@ -1,5 +1,6 @@
 export * from "./api/api_gateway/auth";
 export * from "./api/api_gateway/bootstrap";
+export * from "./api/api_gateway/bootstrap_standalone";
 export * from "./api/api_gateway/bootstrap_v2";
 export * from "./api/api_gateway/contract";
 export * from "./api/api_gateway/esb";
