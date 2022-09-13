a4b01c01e2f4d06b3d601988d2621a5e49f17fda
easyops-eve <easyops-eve@users.noreply.github.com>
2022-09-13 14:25:55 +0800

    feat(): add apis: CmdbObjectApi_collect, CmdbObjectApi_searchCollect
    INFRA-0

---
+++ sdk/cmdb-sdk/src/api/cmdb/cmdb_object/collect.ts
@@ -0,0 +1,23 @@
+import { http, HttpOptions } from "@next-core/brick-http";
+
+export interface CmdbObjectApi_CollectRequestBody {
+  /** 模型id列表 */
+  objectIdList: string[];
+
+  /** 是否收藏 */
+  isCollect: boolean;
+}
+
+/**
+ * @description 收藏或者取消收藏模型
+ * @endpoint POST /object/collect
+ */
+export const CmdbObjectApi_collect = (
+  data: CmdbObjectApi_CollectRequestBody,
+  options?: HttpOptions
+): Promise<void> =>
+  /**! @contract easyops.api.cmdb.cmdb_object.collect@1.0.0 */ http.post<void>(
+    "api/gateway/cmdb.cmdb_object.collect/object/collect",
+    data,
+    options
+  );
